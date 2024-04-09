e6419b58ec152a967053171997dcdab9b9d1574d
Juan Oliveira <juanoliveira@pop-os.localdomain>
2024-04-09 19:12:03 -0300

    created scraper class

---
+++ scraper.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from mycalendar import Calendar
+
+class Scraper:
+    def __init__(self, url) -> None:
+        self.URL = url
+        self.soup = None
+        self.date = Calendar().date
+    
+    def make_soup(self):
+        response = requests.get(self.URL, timeout=10)
+        response.raise_for_status()
+        self.soup = BeautifulSoup(response.text, "lxml")
+    
+
+    def get_info(self):
+        days = self.soup.select(selector="tr")
+        for day in days:
+            if day.strong.text == self.date:
+                return day.find_all("td")
